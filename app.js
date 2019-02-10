c3b44065e1e30be1b87cfc96f44cb5d01c534949
Joe Ashworth <joeashworth@gmail.com>
2019-02-10 11:10:25 -0600

    Set up global controller with TODOs

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
 
-  var x = 23;
-
-  var add = function (a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function (b) {
-      console.log(add(b));
-    }
-  }
+  //
 
 })();
+
+
+
+// UI CONTROLLER
+var UIController = (function () {
+
+  //
+
+})();
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+  //
+  document.querySelector('.add__btn').addEventListener('click', function () {
+
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  });
+
+
+})(budgetController, UIController);
