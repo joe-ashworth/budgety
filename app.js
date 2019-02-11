862efa2a4903ab819fffdc91ba11b9fc317c5809
Joe Ashworth <joeashworth@gmail.com>
2019-02-10 20:36:19 -0600

    Get field input data

--- app.js
+++ app.js
@@ -10,7 +10,26 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
-  //
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getinput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
+  }
 
 })();
 
@@ -19,9 +38,13 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function () {
 
     // 1. Get the field input data
+    var input = UICtrl.getinput();
+    console.log(input);
 
     // 2. Add the item to the budget controller
 
@@ -34,7 +57,7 @@ var controller = (function (budgetCtrl, UICtrl) {
   }
 
   //
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function (event) {
     if (event.keyCode === 13 || event.which === 13) {
